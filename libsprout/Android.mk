6fe079fa22828e9fef8105d4a69f941e06d2b6b0
Varun Chitre <varun.chitre15@gmail.com>
2017-09-30 18:50:06 +0000

    sprout: libsprout: add wpa_supplicant shim
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- libsprout/Android.mk
+++ libsprout/Android.mk
@@ -25,12 +25,13 @@ LOCAL_SRC_FILES := \
     ui.cpp \
     xlog.c \
     xlog_hack.cpp \
-    mtk_hack.cpp
+    mtk_hack.cpp \
+    sprout_wpa_supplicant.cpp
 
 LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
 LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
                           libicuuc libicui18n libcrypto libstagefright_foundation libc
-LOCAL_MODULE := libsprout
+LOCAL_MODULE := libsprout_shims
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CLANG := true

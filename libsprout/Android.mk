SamarV-121/android_device_google_sprout

sprout: libsprout: add wpa_supplicant shim
Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

libsprout/Android.mk
@@ -16,10 +16,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    sprout_ril.cpp
+    sprout_ril.cpp \
+    sprout_wpa_supplicant.cpp
 
 LOCAL_SHARED_LIBRARIES := libbinder
-LOCAL_MODULE := libsprout
+LOCAL_MODULE := libsprout_shims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)

maharudra108/android_device_micromax_a106

libsprout/Android.mk
@@ -25,12 +25,13 @@ LOCAL_SRC_FILES := \
     ui.cpp \
     xlog.c \
     xlog_hack.cpp \
-    mtk_hack.cpp
+    mtk_hack.cpp \
+    sprout_wpa_supplicant.cpp
 
 LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
 LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
                           libicuuc libicui18n libcrypto libstagefright_foundation libc
-LOCAL_MODULE := libsprout
+LOCAL_MODULE := libsprout_shims
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CLANG := true
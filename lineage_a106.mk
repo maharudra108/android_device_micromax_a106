38726e8bcd55039e0986caf77a5b3c56233bbd9f
Manjot Sidhu <manjot.gni@gmail.com>
2018-03-16 14:16:09 +0530

    a106: HIDL manifest and HALs

--- lineage_a106.mk
+++ lineage_a106.mk
@@ -93,7 +93,13 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler \
-    tinymix
+    tinymix \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+    
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -106,8 +112,6 @@ PRODUCT_PACKAGES += \
 # FM Radio
 PRODUCT_PACKAGES += \
     FMRadio \
-    FMRadioGoogle \
-    FmRadioTrampoline2
 
 # GPS
 PRODUCT_PACKAGES += \
@@ -117,10 +121,28 @@ PRODUCT_PACKAGES += \
  PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
+    wificond \
     dhcpcd.conf \
     wpa_supplicant \
-    wpa_supplicant.conf
+    wpa_supplicant.conf \
+    android.hardware.wifi@1.0-service
 
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+
+# Shims
 PRODUCT_PACKAGES += \
     libsprout \
     libxlog
@@ -143,6 +165,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.default \
     power.mt6582
+
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
 # Dynamically set props
 #PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
@@ -176,8 +202,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 	dalvik.vm.image-dex2oat-Xms=64m \
 	dalvik.vm.image-dex2oat-Xmx=64m \
 	ro.dalvik.vm.native.bridge=0 \
-	ro.hardware=sprout \
-	ro.telephony.ril_class=MediaTekRIL
+	ro.telephony.ril_class=SproutRIL \
+        camera.disable_zsl_mode=1
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     pm.dexopt.first-boot=verify-at-runtime \

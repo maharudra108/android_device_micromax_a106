9eedf89a5a14f0ccdfcf3dd79ee2c5b9c1b46524
Manjot Sidhu <manjot.gni@gmail.com>
2018-03-17 20:22:22 +0530

    a106: update patches

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd ../../../../
 cd system/core
-patch -p1 < system_core.patch
-patch -p1 < mkbootimg_1.patch
-patch -p1 < mkbootimg_2.patch
-patch -p1 < mkbootimg_3.patch
+patch -p1 < ../../device/micromax/a106/system_core.patch
+patch -p1 < ../../device/micromax/a106/mkbootimg_1.patch
+patch -p1 < ../../device/micromax/a106/mkbootimg_2.patch
+patch -p1 < ../../device/micromax/a106/mkbootimg_3.patch
+cd ../../
+cd hardware/interfaces
+patch -p1 < ../../device/micromax/a106/patches/hardware_interfaces.patch
 cd ../../

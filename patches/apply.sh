49fd9d873f4ce28adaaba196ae10a0e525370ed2
Manjot Sidhu <manjot.gni@gmail.com>
2018-03-21 17:25:07 +0530

    patches: fix derps

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ../../../../
 cd system/core
-patch -p1 < ../../device/micromax/a106/system_core_1.patch
-patch -p1 < ../../device/micromax/a106/system_core_2.patch
-patch -p1 < ../../device/micromax/a106/mkbootimg_1.patch
-patch -p1 < ../../device/micromax/a106/mkbootimg_2.patch
-patch -p1 < ../../device/micromax/a106/mkbootimg_3.patch
+patch -p1 < ../../device/micromax/a106/patches/system_core_1.patch
+patch -p1 < ../../device/micromax/a106/patches/system_core_2.patch
+patch -p1 < ../../device/micromax/a106/patches/mkbootimg_1.patch
+patch -p1 < ../../device/micromax/a106/patches/mkbootimg_2.patch
+patch -p1 < ../../device/micromax/a106/patches/mkbootimg_3.patch
 cd ../../
 cd hardware/interfaces
 patch -p1 < ../../device/micromax/a106/patches/hardware_interfaces.patch
 cd ../../
 cd bionic/
-patch -p1 < ../../device/micromax/a106/patches/bionic.patch
+patch -p1 < ../device/micromax/a106/patches/bionic.patch
 cd ../

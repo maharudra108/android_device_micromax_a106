8e9de85f9b704a4c9609933a42c839a8f1d1d2fa
Manjot Sidhu <manjot.gni@gmail.com>
2018-03-21 17:11:06 +0530

    patches: update

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ../../../../
 cd system/core
-patch -p1 < ../../device/micromax/a106/system_core.patch
+patch -p1 < ../../device/micromax/a106/system_core_1.patch
+patch -p1 < ../../device/micromax/a106/system_core_2.patch
 patch -p1 < ../../device/micromax/a106/mkbootimg_1.patch
 patch -p1 < ../../device/micromax/a106/mkbootimg_2.patch
 patch -p1 < ../../device/micromax/a106/mkbootimg_3.patch

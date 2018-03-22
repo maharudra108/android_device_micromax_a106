054250c0d8fb09f8f78ea9b4957a0755268696bf
Manjot Sidhu <manjot.gni@gmail.com>
2018-03-22 10:11:16 +0530

    libsprout: add another missing symbol from libaudiopolicyservice  F libc    : CANNOT LINK EXECUTABLE "/system/bin/audioserver": cannot locate symbol "_ZN7android11AudioEffect12stringToGuidEPKcP13effect_uuid_s" referenced by "/system/lib/libaudiopolicyservice.so"

--- libsprout/audio.cpp
+++ libsprout/audio.cpp
@@ -2,3 +2,6 @@ extern "C" {
  void _ZN7android11AudioEffectC1EPK13effect_uuid_sRKNS_8String16ES3_iPFviPvS7_ES7_15audio_session_ti(){}
 }
 
+extern "C" {
+ void _ZN7android11AudioEffect12stringToGuidEPKcP13effect_uuid_s(){}
+}
